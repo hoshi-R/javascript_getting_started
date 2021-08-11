50a09c103c3a480ae4931d8cc9fdcf81e8b8fb73
hoshi-R <hs.rt7771@gmail.com>
2021-08-11 21:29:55 +0900

    更新

--- scripts.js
+++ scripts.js
@@ -234,4 +234,11 @@
     result_1.textContent = count;
   });
 
+  for (let i = 0; i < 3; i++) {
+    for ( let j = 0; j < 3; j++) {
+      console.count('LOOP');
+    }
+  }
+  console.count('LOOP');
+
 })();
